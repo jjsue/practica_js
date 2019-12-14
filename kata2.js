092af5a409d3151f6e5c9fac927f02d6449fafa7
jjsue <jjsue_95@hotmail.es>
2019-12-14 22:11:51 +0100

    Borro variables que no sirven para nada. De momento mi función para pasar de arabes a romanos solo va restando y añadiendo numeros a la derecha, por lo que ahora tengo que validar los numeros.

--- kata2.js
+++ kata2.js
@@ -36,53 +36,39 @@ Sólo se contemplan números entre el 1 y el 3999
 * Hacer un validador de números romanos*/
 
 function fromArabToRoman(number){
-    let countI = 0; // 1 / R:3
-    let countV = 0; // 5 / R: NO
-    let countX = 0; // 10 / R: 3
-    let countL = 0; // 50 / R: NO
-    let countC = 0; // 100 /: 3
-    let countD = 0; // 500 / R: NO
-    let countM = 0; // 1000 / R: 3
     let romanString = '';
     while (number != 0){
         if (number >= 1000){
-            countM++;
             number -= 1000;
             romanString += 'M';
             continue;
         }
         if (number >= 500){
-            countD++;
             number -= 500;
             romanString += 'D';
             continue;
         }
         if (number >= 100 ){
-            countC++;
             number -= 100;
             romanString += 'C';
             continue;
         }
         if (number >= 50){
-            countL++;
             number -= 50;
             romanString += 'L';
             continue;
         }
         if (number >= 10){
-            countX++;
             number -= 10;
             romanString += 'X';
             continue;
         }
         if (number >= 5){
-            countV++;
             number -= 5;
             romanString += 'V';
             continue;
         }
         if (number >= 1){
-            countI++;
             number -= 1;
             romanString += 'I';
             continue;
