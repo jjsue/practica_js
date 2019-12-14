755be59d25cd63185647313ac0348327d53032ff
jjsue <jjsue_95@hotmail.es>
2019-12-14 19:05:21 +0100

    Me faltaba una parte del ejercicio uno y me he dado cuenta mas tarde, la parte de que contenga el numero no la habia realizado y ahora si.

--- kata1.js
+++ kata1.js
@@ -1,24 +1,30 @@
 function fooBarQuix(num){
     let myString = ''
-    let controlWritte = false;
+    let numString = num.toString();
     if (num % 3 === 0){
         myString = myString + "Foo";
-        controlWritte = true;
     }
     if (num % 5 === 0){
         myString = myString + "Bar";
-        controlWritte = true;
     }
     if (num % 7 === 0){
         myString = myString + "Quix";
-        controlWritte = true;
     }
-    if (controlWritte){
+    for (let index = 0; index < numString.length; index++) {
+        if(numString[index] === '3'){
+            myString = myString + "Foo";
+        }
+        if(numString[index] === '5'){
+            myString = myString + "Bar";
+        }
+        if(numString[index] === '7'){
+            myString = myString + "Quix";
+        }
+    }
+    if (myString != ''){ //Si se ha escrito algo devolvemos la string creada.
         return myString;
     }
-    else{
-        return num;
-    }
+    return num; // Si no pues directamente devolvemos el numero.
 }
 let number = Math.floor((Math.random() * 100) + 1);
 console.log(`El numero elegido es el: ${number}`);
