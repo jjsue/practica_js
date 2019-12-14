1d0eb81e22c6eaeab3a887a50b93344171380a47
jjsue <jjsue_95@hotmail.es>
2019-12-14 17:04:58 +0100

    Descomentamos el kata 1.

--- kata1.js
+++ kata1.js
@@ -1,13 +1,3 @@
-/*## FooBarQuix
-
-Nos dan un número entre el 1 y 100, y tenemos que devolver por orden lo siguiente:
-
-* Si el número es divisible por 3, escribiremos “Foo” en lugar del número
-* Si el número es divisible por 5, añadimos “Bar”
-* Si el número es divisible por 7, añadimos “Quix”.
-* Por cada dígito 3,5 o 7, añadiremos “Foo”, “Bar”, “Quix” respectivamente y en orden de aparición.*/
-
-let number = Math.floor((Math.random() * 100) + 1); //Obtengo numero aleatorio 1 al 100.
 function fooBarQuix(num){
     let myString = ''
     let controlWritte = false;
@@ -30,6 +20,6 @@ function fooBarQuix(num){
         return num;
     }
 }
-
+let number = Math.floor((Math.random() * 100) + 1);
 console.log(`El numero elegido es el: ${number}`);
 console.log(fooBarQuix(number));
