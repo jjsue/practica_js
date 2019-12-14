b064304532e405fc29f591106bdcec3de9bb7b18
jjsue <jjsue_95@hotmail.es>
2019-12-14 13:50:36 +0100

    Copiamos el enunciado y creamos el branch de la kata 1

---
+++ kata1.js
@@ -0,0 +1,8 @@
+/*## FooBarQuix
+
+Nos dan un número entre el 1 y 100, y tenemos que devolver por orden lo siguiente:
+
+* Si el número es divisible por 3, escribiremos “Foo” en lugar del número
+* Si el número es divisible por 5, añadimos “Bar”
+* Si el número es divisible por 7, añadimos “Quix”.
+* Por cada dígito 3,5 o 7, añadiremos “Foo”, “Bar”, “Quix” respectivamente y en orden de aparición.*/
