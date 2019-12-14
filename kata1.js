495ffb09305ad6b389c7fe5418e6a8559ce59184
jjsue <jjsue_95@hotmail.es>
2019-12-14 17:03:32 +0100

    Finalizado Kata 1

--- kata1.js
+++ kata1.js
@@ -6,3 +6,30 @@ Nos dan un número entre el 1 y 100, y tenemos que devolver por orden lo siguien
 * Si el número es divisible por 5, añadimos “Bar”
 * Si el número es divisible por 7, añadimos “Quix”.
 * Por cada dígito 3,5 o 7, añadiremos “Foo”, “Bar”, “Quix” respectivamente y en orden de aparición.*/
+
+let number = Math.floor((Math.random() * 100) + 1); //Obtengo numero aleatorio 1 al 100.
+function fooBarQuix(num){
+    let myString = ''
+    let controlWritte = false;
+    if (num % 3 === 0){
+        myString = myString + "Foo";
+        controlWritte = true;
+    }
+    if (num % 5 === 0){
+        myString = myString + "Bar";
+        controlWritte = true;
+    }
+    if (num % 7 === 0){
+        myString = myString + "Quix";
+        controlWritte = true;
+    }
+    if (controlWritte){
+        return myString;
+    }
+    else{
+        return num;
+    }
+}
+
+console.log(`El numero elegido es el: ${number}`);
+console.log(fooBarQuix(number));
