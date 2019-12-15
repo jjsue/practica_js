7761095d0ac1949192c0c6b41eb1a032aab6cacf
jjsue <jjsue_95@hotmail.es>
2019-12-15 12:38:32 +0100

    Añadimos comentarios.

---
+++ kata2.js
@@ -0,0 +1,24 @@
+/*Reglas
+
+Sólo se contemplan números entre el 1 y el 3999
+
+    Los símbolos I, X, C y M se pueden repetir hasta tres veces.
+    Los símbolos V, L y D no pueden repetirse.
+    Los símbolos I, X y C se suman si están a la derecha de otro mayor o igual.
+    Los símbolos I, X y C se restan si están a la izquierda de otro mayor y solamente pueden anteponerse a los dos símbolos que le siguen en la sucesión.
+    I se resta de V y X
+    X se resta de L y C
+    C se resta de D y M
+    Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.
+Simbolos:
+I : 1
+V : 5
+X : 10
+L : 50
+C : 100
+D : 500
+M : 1000
+    */
+/*function fromArabToRoman(arabNumber){
+    //Creamos un array con el que poder tener el valor en romano y en castellano:
+}*/
