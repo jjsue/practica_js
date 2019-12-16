2f24b2a1b47c8c5ef5271e0ab5763a4c525897c4
jjsue <jjsue_95@hotmail.es>
2019-12-16 12:09:47 +0100

    Ya funcionan las unidades del conversor de arabes a romanos, soy feliz

--- kata2.js
+++ kata2.js
@@ -17,8 +17,63 @@ X : 10
 L : 50
 C : 100
 D : 500
-M : 1000
-    */
-/*function fromArabToRoman(arabNumber){
-    //Creamos un array con el que poder tener el valor en romano y en castellano:
-}*/
+M : 1000*/
+
+function reverse(string) { //Funcion que me da la vuelta a una cadena.
+    let x = string.length;
+    let reverseString = '';
+   
+    while (x>=0) {
+      reverseString = reverseString + string.charAt(x);
+      x--;
+    }
+    return reverseString;
+}
+function fromArabToRoman(arabNumber){
+    //Lo iba a hacer con diccionarios pero creo que va a ser mejor con array.
+    const arabString = reverse(arabNumber.toString());
+    let unidades = '';
+    let decenas = '';
+    let centenas = '';
+    let millares = '';
+    let romanString = '';
+    for (let i = 0; i < arabString.length; i++) {
+        if (i === 0){ // Aquí estamos en las unidades.
+            switch(arabString[i]) {
+                case '1':
+                    unidades = 'I';
+                    break;
+                case '2':
+                    unidades = 'II';
+                    break;
+                case '3':
+                    unidades = 'III';
+                    break;
+                case '4':
+                    unidades = 'IV';
+                    break;
+                case '5':
+                    unidades = 'V';
+                    break;
+                case '6':
+                    unidades = 'VI';
+                    break;
+                case '7':
+                    unidades = 'VII'
+                    break;
+                case '8':
+                    unidades = 'VIII';
+                    break;
+                case '9':
+                    unidades = 'IX';
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    romanString = millares + centenas + decenas + unidades;
+    return romanString;
+}
+
+console.log(fromArabToRoman(9));
