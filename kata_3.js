200be3301fd4d5501758a384a72b39480bee84f3
jjsue <jjsue_95@hotmail.es>
2019-12-20 14:15:22 +0100

    Ya tengo una versión primitiva del programa. Lo que falta ya es que el programa identifique al ganador

--- kata_3.js
+++ kata_3.js
@@ -76,8 +76,35 @@ function pokerRules(player){
         console.log("Tiene escalera de color");
     }
     // Vamos con el four of a kind o poker.
-    if ((player[0].value === player[1].value && player[0].value === player[2].value && player[0].value === player[3].value) || (player[1].value === player[2].value && player[1].value === player[3].value && player[1].value === player[4].value)){
+    else if ((player[0].value === player[1].value && player[0].value === player[2].value && player[0].value === player[3].value) || (player[1].value === player[2].value && player[1].value === player[3].value && player[1].value === player[4].value)){
         console.log("Tiene poker");
+    }
+    //Full house. Recordemos que gana el que tenga el trio mas alto.
+    else if ((player[0].value === player[1].value && player[0].value === player[2].value && player[3].value === player[4].value) || (player[0].value === player[1].value && player[2].value === player[3].value && player[2].value === player[4].value)){
+        console.log("Tiene full house");
+    }
+    //Flush, Color, todas cartas mismo palo. Si hay empate gana carta mas alta.
+    else if (player[0].palo === player[1].palo && player[0].palo === player[2].palo && player[0].palo === player[3].palo && player[0].palo === player[4].palo){
+        console.log("Tiene flush, color");
+    }
+    //Escalera, todas las cartas consecutivas pero sin ser del mismo palo. Si hay empate gana carta mas alta.
+    else if (player[0].value + 1 === player[1].value && player[1].value + 1 === player[2].value && player[2].value + 1 === player[3].value && player[3].value + 1 === player[4].value){
+        console.log ("Tiene escalera sin color");
+    }
+    //Vamos con el trio. Si hay empate gana el que tenga el trio de la carta mas alta.
+    else if ((player[0].value === player[1].value && player[0].value === player[2].value) || (player[1].value === player[2].value && player[1].value === player[3].value) || (player[2].value === player[3].value && player[2].value === player[4].value)){
+        console.log("Tiene trio");
+    }
+    //Dobles parejas. Empate gana la pareja mas alta, si las dos parejas son igual de altas se compara la otra pareja, si no, la restante.
+    else if((player[0].value === player[1].value && player[2].value === player[3].value) || (player[1].value === player[2].value && player[3].value === player[4].value) || (player[0].value === player[1].value && player[3].value === player[4].value)){
+        console.log("Dobles parejas");
+    }
+    // Parejas
+    else if(player[0].value === player[1].value || player[1].value === player[2].value || player[2].value === player[3].value || player[3].value === player[4].value){
+        console.log("Parejas");
+    }
+    else{
+        console.log("Carta mas alta.");
     }
 }
 
@@ -89,16 +116,16 @@ function pokerGame(){
                 {palo: 'D', value: 2}, {palo: 'D', value: 3}, {palo: 'D', value: 4}, {palo: 'D', value: 5}, {palo: 'D', value: 6}, {palo: 'D', value: 7}, {palo: 'D', value: 8}, {palo: 'D', value: 9}, {palo: 'D', value: 'T'}, {palo: 'D', value: 'J'}, {palo: 'D', value: 'Q'}, {palo: 'D', value: 'K'}, {palo: 'D', value: 'A'}];
     const mixed = shuffle(deck); //Se baraja.
     //Ahora vamos a dar las manos.
-    /*for (let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++){
         if (i % 2 === 1){
             playerOne.push(mixed[i]);
         }
         if (i % 2 === 0){
             playerTwo.push(mixed[i]);
         }
-    }*/
-    playerOne = [{palo: 'S', value: 'T'}, {palo: 'H', value: 'T'}, {palo: 'S', value: 'A'}, {palo: 'C', value: 'T'}, {palo: 'D', value: 'T'}];
+    }
     //Con las manos ya barajadas toca ver quien gana. Vamos a crear una función a parte para ordenarlo un poco.
+    console.log(playerOne);
     pokerRules(playerOne);
 }
 pokerGame();
